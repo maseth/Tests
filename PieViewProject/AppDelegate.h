dd1bcc304bcea19060f4571e1bb7edcab03288b7
maseth <maciej@gregorczyk.net>
2013-02-02 22:15:34 +0100

    Initial Commit

---
+++ PieViewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PieViewProject
+//
+//  Created by Maciej Gregorczyk on 02.02.2013.
+//  Copyright (c) 2013 Maciej Gregorczyk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PieViewProject/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PieViewProject
+//
+//  Created by Maciej Gregorczyk on 02.02.2013.
+//  Copyright (c) 2013 Maciej Gregorczyk. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
